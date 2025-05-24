6dc755f5499cf773132e2ca68516515666a88a2f
Lee Coppens <lee.coppens@hotmail.be>
2025-05-24 15:55:45 +0200

    General improvements.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import {defineConfig} from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import vue from '@astrojs/vue';
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()]
-    },
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [vue()],
 });
---
+++ src/components/pageElements/AnimatedSplitWords.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="split-words">
+    <slot/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {gsap} from "gsap";
+import {SplitText} from "gsap/SplitText";
+import {onMounted} from "vue";
+
+gsap.registerPlugin(SplitText);
+
+onMounted(() => {
+  let split = SplitText.create(".split-words", {type: "words"});
+
+  gsap.from(split.words, {
+    y: -100,
+    opacity: 0,
+    rotation: "random(-80, 80)",
+    duration: 0.7,
+    ease: "back",
+    stagger: 0.05
+  })
+})
+</script>
